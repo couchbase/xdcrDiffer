f5c3b252b9370c260d415059f4757caf2de254e6
Neil Huang <neil.huang@couchbase.com>
2018-09-15 17:07:37 -0700

    Differ algorithm with unit test

---
+++ differ/differ.go
@@ -0,0 +1,346 @@
+package xdcrDiffer
+
+import (
+	"crypto/sha512"
+	"encoding/binary"
+	"encoding/hex"
+	"encoding/json"
+	"fmt"
+	"io"
+	"os"
+	"sort"
+	"strings"
+	"sync"
+)
+
+// Given two DCP Dump files, perform necessary diffing task
+type FilesDiffer struct {
+	file1 FileAttributes
+	file2 FileAttributes
+
+	// errors corresponding to the attributes ops
+	err1 error
+	err2 error
+
+	// for parallelism
+	dataLoadWg sync.WaitGroup
+
+	// Diff results
+	MissingFromFile1     []*oneEntry
+	MissingFromFile2     []*oneEntry
+	BothExistButMismatch []*entryPair
+}
+
+type FileAttributes struct {
+	name          string
+	entries       map[string]*oneEntry
+	sortedEntries []*oneEntry
+}
+
+func NewFileAttribute(fileName string) *FileAttributes {
+	attr := &FileAttributes{
+		name:    fileName,
+		entries: make(map[string]*oneEntry),
+	}
+	return attr
+}
+
+type oneEntry struct {
+	Key      string
+	Seqno    uint64
+	RevId    uint64
+	Cas      uint64
+	Expiry   uint64
+	BodyHash [sha512.Size]byte
+}
+
+func (oneEntry *oneEntry) String() string {
+	return fmt.Sprintf("Key: %v Seqno: %v RevId: %v Cas: %v Expiry: %v Hash: %s",
+		oneEntry.Key, oneEntry.Seqno, oneEntry.RevId, oneEntry.Cas, oneEntry.Expiry, hex.EncodeToString(oneEntry.BodyHash[:]))
+}
+
+type entryPair struct {
+	A *oneEntry
+	B *oneEntry
+}
+
+type ByKeyName []*oneEntry
+
+func shaCompare(a, b [sha512.Size]byte) bool {
+	for i := 0; i < sha512.Size; i++ {
+		if a[i] != b[i] {
+			return false
+		}
+	}
+	return true
+}
+
+// Note Expiry is not used for conflict resolution
+// Returns a boolean to showcase if the values all match
+// For int return val:
+//  0 - Names are the same
+//  1 - If entry name > other name
+// -1 - If entry name < other name
+func (entry oneEntry) Diff(other oneEntry) (int, bool) {
+	if entry.Key != other.Key {
+		if entry.Key > other.Key {
+			return 1, false
+		} else {
+			return -1, false
+		}
+	} else if entry.RevId != other.RevId {
+		return 0, false
+	} else if entry.Cas != other.Cas {
+		return 0, false
+	} else if !shaCompare(entry.BodyHash, other.BodyHash) {
+		return 0, false
+	}
+	return 0, true
+}
+
+func NewFilesDiffer(file1, file2 string) *FilesDiffer {
+	differ := &FilesDiffer{
+		file1: *NewFileAttribute(file1),
+		file2: *NewFileAttribute(file2),
+	}
+	return differ
+}
+
+func getOneEntry(fileHandle *os.File) (*oneEntry, error) {
+	entry := &oneEntry{}
+
+	keyLenBytes := make([]byte, 2)
+	bytesRead, err := fileHandle.Read(keyLenBytes)
+	if err != nil {
+		return nil, fmt.Errorf("Unable to read keyLen, bytes read: %v, err: %v", bytesRead, err)
+	}
+	entryKeyLen := binary.BigEndian.Uint16(keyLenBytes)
+
+	keyBytes := make([]byte, entryKeyLen)
+	bytesRead, err = fileHandle.Read(keyBytes)
+	if err != nil {
+		return nil, fmt.Errorf("Unable to read key, bytes read: %v, err: %v", bytesRead, err)
+	}
+	entry.Key = string(keyBytes)
+
+	seqnoBytes := make([]byte, 8)
+	bytesRead, err = fileHandle.Read(seqnoBytes)
+	if err != nil {
+		return nil, fmt.Errorf("Unable to read seqno, bytes read: %v, err: %v", bytesRead, err)
+	}
+	entry.Seqno = binary.BigEndian.Uint64(seqnoBytes)
+
+	revIdBytes := make([]byte, 8)
+	bytesRead, err = fileHandle.Read(revIdBytes)
+	if err != nil {
+		return nil, fmt.Errorf("Unable to read revIdBytes, bytes read: %v, err: %v", bytesRead, err)
+	}
+	entry.RevId = binary.BigEndian.Uint64(revIdBytes)
+
+	casBytes := make([]byte, 8)
+	bytesRead, err = fileHandle.Read(casBytes)
+	if err != nil {
+		return nil, fmt.Errorf("Unable to read casBytes, bytes read: %v, err: %v", bytesRead, err)
+	}
+	entry.Cas = binary.BigEndian.Uint64(casBytes)
+
+	expiryBytes := make([]byte, 8)
+	bytesRead, err = fileHandle.Read(expiryBytes)
+	if err != nil {
+		return nil, fmt.Errorf("Unable to read expiryBytes, bytes read: %v, err: %v", bytesRead, err)
+	}
+	entry.Expiry = binary.BigEndian.Uint64(expiryBytes)
+
+	hashBytes := make([]byte, sha512.Size)
+	bytesRead, err = fileHandle.Read(hashBytes)
+	if err != nil {
+		return nil, fmt.Errorf("Unable to read hashBytes, bytes read: %v, err: %v", bytesRead, err)
+	}
+	copy(entry.BodyHash[:], hashBytes)
+
+	return entry, nil
+}
+
+func (a ByKeyName) Len() int           { return len(a) }
+func (a ByKeyName) Swap(i, j int)      { *a[i], *a[j] = *a[j], *a[i] }
+func (a ByKeyName) Less(i, j int) bool { return a[i].Key < a[j].Key }
+
+func (attr *FileAttributes) fillAndDedupEntries(fileHandle *os.File) error {
+	var err error
+	var entry *oneEntry
+
+	for err == nil {
+		entry, err = getOneEntry(fileHandle)
+		if err != nil {
+			break
+		}
+
+		if curEntry, ok := attr.entries[entry.Key]; !ok {
+			attr.entries[entry.Key] = entry
+		} else {
+			// Replace the entry in the map if the seqno is newer
+			if entry.Seqno > curEntry.Seqno {
+				attr.entries[entry.Key] = entry
+			}
+		}
+	}
+
+	if err != nil && strings.Contains(err.Error(), io.EOF.Error()) {
+		err = nil
+	}
+
+	return err
+}
+
+func (attr *FileAttributes) sortEntries() {
+	for _, v := range attr.entries {
+		attr.sortedEntries = append(attr.sortedEntries, v)
+	}
+
+	sort.Sort(ByKeyName(attr.sortedEntries))
+}
+
+func (attr *FileAttributes) LoadFileIntoBuffer() error {
+	if len(attr.name) == 0 {
+		return fmt.Errorf("No file specified")
+	}
+	file, err := os.Open(attr.name)
+	defer file.Close()
+	if err != nil {
+		return err
+	}
+	err = attr.fillAndDedupEntries(file)
+	if err != nil {
+		return err
+	}
+	attr.sortEntries()
+	return nil
+}
+
+func (differ *FilesDiffer) asyncLoad(attr *FileAttributes, err *error) {
+	defer differ.dataLoadWg.Done()
+	*err = attr.LoadFileIntoBuffer()
+}
+
+func (differ *FilesDiffer) diffSorted() bool {
+	file1Len := len(differ.file1.sortedEntries)
+	file2Len := len(differ.file2.sortedEntries)
+
+	if file1Len == 0 && file2Len == 0 {
+		return true
+	}
+
+	var i int
+	var j int
+
+	for i < file1Len && j < file2Len {
+		item1 := differ.file1.sortedEntries[i]
+		item2 := differ.file2.sortedEntries[j]
+
+		keyCompare, match := item1.Diff(*item2)
+		if match {
+			// Both items are the same
+			i++
+			j++
+		} else {
+			if keyCompare == 0 {
+				// Both document are the same, but others mismatched
+				pair := &entryPair{
+					A: item1,
+					B: item2,
+				}
+				differ.BothExistButMismatch = append(differ.BothExistButMismatch, pair)
+				i++
+				j++
+			} else if keyCompare < 0 {
+				// Like "a" < "b", where a is 1 and b is 2
+				differ.MissingFromFile2 = append(differ.MissingFromFile2, item1)
+				i++
+			} else {
+				// "b" > "a", leading to keyCompare > 0
+				differ.MissingFromFile1 = append(differ.MissingFromFile1, item2)
+				j++
+			}
+		}
+	}
+
+	for ; i < file1Len; i++ {
+		// This means that all the rest of the entries in file1 are missing from file2
+		differ.MissingFromFile2 = append(differ.MissingFromFile2, differ.file1.sortedEntries[i])
+	}
+
+	for ; j < file2Len; j++ {
+		// This means that all the rest of the entries in file2 are missing from file1
+		differ.MissingFromFile1 = append(differ.MissingFromFile1, differ.file1.sortedEntries[j])
+	}
+
+	return len(differ.BothExistButMismatch) == 0 && len(differ.MissingFromFile1) == 0 && len(differ.MissingFromFile2) == 0
+}
+
+// Returns true if they are the same
+func (differ *FilesDiffer) Diff() bool {
+	differ.dataLoadWg.Add(1)
+	go differ.asyncLoad(&differ.file1, &differ.err1)
+	differ.dataLoadWg.Add(1)
+	go differ.asyncLoad(&differ.file2, &differ.err2)
+	differ.dataLoadWg.Wait()
+
+	if differ.err1 != nil {
+		fmt.Printf("Error when loading file1 contents: %v\n", differ.err1)
+	}
+	if differ.err2 != nil {
+		fmt.Printf("Error when loading file2 contents: %v\n", differ.err2)
+	}
+
+	return differ.diffSorted()
+}
+
+func (differ *FilesDiffer) PrettyPrintResult() {
+	mismatchCnt := len(differ.BothExistButMismatch)
+	missing1Cnt := len(differ.MissingFromFile1)
+	missing2Cnt := len(differ.MissingFromFile2)
+
+	if len(differ.file1.entries) == 0 && len(differ.file2.entries) == 0 {
+		fmt.Printf("Diff tool has not been run yet\n")
+	} else if mismatchCnt == 0 && missing1Cnt == 0 && missing2Cnt == 0 {
+		fmt.Printf("Both sides match\n")
+	} else {
+		if mismatchCnt > 0 {
+			fmt.Printf("%v Docs exist in both files but mismatch:\n", mismatchCnt)
+			fmt.Printf("--------------------------------------\n")
+			for i := 0; i < mismatchCnt; i++ {
+				fmt.Printf("File1: %v\n", differ.BothExistButMismatch[i].A.String())
+				fmt.Printf("File2: %v\n", differ.BothExistButMismatch[i].B.String())
+			}
+			fmt.Printf("--------------------------------------\n")
+		}
+		if missing2Cnt > 0 {
+			fmt.Printf("%v Docs exist in file 1 that are missing from file2:\n", missing2Cnt)
+			fmt.Printf("-------------------------------------------------\n")
+			for i := 0; i < missing2Cnt; i++ {
+				fmt.Printf("%v\n", differ.MissingFromFile2[i].String())
+			}
+			fmt.Printf("-------------------------------------------------\n")
+		}
+		if missing1Cnt > 0 {
+			fmt.Printf("%v Docs exist in file 2 that are missing from file1:\n", missing1Cnt)
+			fmt.Printf("-------------------------------------------------\n")
+			for i := 0; i < missing1Cnt; i++ {
+				fmt.Printf("%v\n", differ.MissingFromFile1[i].String())
+			}
+			fmt.Printf("-------------------------------------------------\n")
+		}
+	}
+}
+
+func (differ *FilesDiffer) ToJson() ([]byte, error) {
+	outputMap := map[string]interface{}{
+		"Mismatch":         differ.BothExistButMismatch,
+		"MissingFromFile1": differ.MissingFromFile1,
+		"MissingFromFile2": differ.MissingFromFile2,
+	}
+
+	ret, err := json.Marshal(outputMap)
+
+	return ret, err
+}
---
+++ differ/differ_test.go
@@ -0,0 +1,291 @@
+package xdcrDiffer
+
+import (
+	"crypto/sha512"
+	"encoding/binary"
+	"fmt"
+	"github.com/stretchr/testify/assert"
+	"io/ioutil"
+	"math/rand"
+	"os"
+	"sync"
+	"testing"
+	"time"
+)
+
+const MaxUint64 = ^uint64(0)
+const MinUint = 0
+
+var randomOnce sync.Once
+
+func randomString(l int) string {
+	bytes := make([]byte, l)
+	for i := 0; i < l; i++ {
+		bytes[i] = byte(randInt(65, 90))
+	}
+	return string(bytes)
+}
+
+func randInt(min int, max int) int {
+	return min + rand.Intn(max-min)
+}
+
+// serialize mutation into []byte
+// format:
+//  keyLen  - 2 bytes
+//  key  - length specified by keyLen
+//  seqno   - 8 bytes
+//  revId   - 8 bytes
+//  cas     - 8 bytes
+//  expiry  - 8 bytes?
+//  hash    - 64 bytes
+func genTestData() (key string, seqno, revId, cas, expiry uint64, hash [sha512.Size]byte, ret []byte) {
+	randomOnce.Do(func() {
+		rand.Seed(time.Now().UTC().UnixNano())
+	})
+
+	key = randomString(randInt(12, 64))
+	seqno = rand.Uint64()
+	revId = rand.Uint64()
+	cas = rand.Uint64()
+	expiry = rand.Uint64()
+	// Note we don't have the actual body hash so just randomly generate a hash using key
+	hash = sha512.Sum512([]byte(key))
+
+	dataSlice := createDataByteSlice(key, seqno, revId, cas, expiry, hash)
+
+	return key, seqno, revId, cas, expiry, hash, dataSlice
+}
+
+func createDataByteSlice(key string, seqno, revId, cas, expiry uint64, hash [sha512.Size]byte) []byte {
+	var keyLen uint16 = uint16(len(key))
+	retLength := 34 + sha512.Size + keyLen
+	ret := make([]byte, retLength)
+
+	// fmt.Printf("%v bytes generated: keyLen: %v key: %v seqno: %v revId: %v cas: %v expiry: %v hash: %v\n",
+	// 	retLength, keyLen, key, seqno, revId, cas, expiry, hash)
+
+	pos := 0
+	binary.BigEndian.PutUint16(ret[pos:pos+2], keyLen)
+	pos += 2
+	copy(ret[pos:pos+int(keyLen)], key)
+	pos += int(keyLen)
+	binary.BigEndian.PutUint64(ret[pos:pos+8], seqno)
+	pos += 8
+	binary.BigEndian.PutUint64(ret[pos:pos+8], revId)
+	pos += 8
+	binary.BigEndian.PutUint64(ret[pos:pos+8], cas)
+	pos += 8
+	binary.BigEndian.PutUint64(ret[pos:pos+8], expiry)
+	pos += 8
+	copy(ret[pos:], hash[:])
+
+	return ret
+}
+
+func genMultipleRecords(numOfRecords int) []byte {
+	var retSlice []byte
+
+	for i := 0; i < numOfRecords; i++ {
+		_, _, _, _, _, _, record := genTestData()
+		retSlice = append(retSlice, record...)
+	}
+
+	return retSlice
+}
+
+func genSameFiles(numOfRecords int, fileName1, fileName2 string) error {
+	data := genMultipleRecords(numOfRecords)
+
+	err := ioutil.WriteFile(fileName1, data, 0644)
+	if err != nil {
+		return err
+	}
+
+	err = ioutil.WriteFile(fileName2, data, 0644)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func genMismatchedFiles(numOfRecords, mismatchCnt int, fileName1, fileName2 string) ([]string, error) {
+	var mismatchedKeyNames []string
+	data := genMultipleRecords(numOfRecords - mismatchCnt)
+
+	err := ioutil.WriteFile(fileName1, data, 0644)
+	if err != nil {
+		return mismatchedKeyNames, err
+	}
+
+	err = ioutil.WriteFile(fileName2, data, 0644)
+	if err != nil {
+		return mismatchedKeyNames, err
+	}
+
+	// Now create mismatched entries
+	f1, err := os.OpenFile(fileName1, os.O_APPEND|os.O_WRONLY, 644)
+	if err != nil {
+		return mismatchedKeyNames, err
+	}
+	defer f1.Close()
+
+	f2, err := os.OpenFile(fileName2, os.O_APPEND|os.O_WRONLY, 644)
+	if err != nil {
+		return mismatchedKeyNames, err
+	}
+	defer f2.Close()
+
+	for i := 0; i < mismatchCnt; i++ {
+		key, seqno, revId, cas, expiry, hash, oneData := genTestData()
+		mismatchedData := createDataByteSlice(key, seqno, revId, cas+1, expiry, hash)
+
+		_, err = f1.Write(oneData)
+		if err != nil {
+			return mismatchedKeyNames, err
+		}
+
+		_, err = f2.Write(mismatchedData)
+		if err != nil {
+			return mismatchedKeyNames, err
+		}
+
+		mismatchedKeyNames = append(mismatchedKeyNames, key)
+	}
+
+	return mismatchedKeyNames, nil
+}
+
+func verifyMisMatch(mismatchKeys []string, differ *FilesDiffer) bool {
+	for _, key := range mismatchKeys {
+		found := false
+		for _, onePair := range differ.BothExistButMismatch {
+			if key == onePair.A.Key {
+				found = true
+				break
+			}
+		}
+		if !found {
+			return false
+		}
+	}
+	return true
+}
+
+func TestLoader(t *testing.T) {
+	assert := assert.New(t)
+	var outputFileTemp string = "/tmp/xdcrDiffer.tmp"
+	defer os.Remove(outputFileTemp)
+
+	key, seqno, _, _, _, _, data := genTestData()
+
+	err := ioutil.WriteFile(outputFileTemp, data, 0644)
+	assert.Nil(err)
+
+	differ := NewFilesDiffer(outputFileTemp, "")
+	err = differ.file1.LoadFileIntoBuffer()
+	assert.Nil(err)
+
+	assert.Equal(1, len(differ.file1.entries))
+	assert.Equal(seqno, differ.file1.entries[key].Seqno)
+
+	assert.Equal(1, len(differ.file1.sortedEntries))
+	assert.Equal(seqno, differ.file1.sortedEntries[0].Seqno)
+}
+
+func TestLoadSameFile(t *testing.T) {
+	fmt.Println("============== Test case start: TestLoadSameFile =================")
+	assert := assert.New(t)
+
+	file1 := "/tmp/test1.bin"
+	file2 := "/tmp/test2.bin"
+	defer os.Remove(file1)
+	defer os.Remove(file2)
+
+	entries := 10000
+
+	err := genSameFiles(entries, file1, file2)
+	assert.Equal(nil, err)
+
+	differ := NewFilesDiffer(file1, file2)
+	assert.NotNil(differ)
+
+	result := differ.Diff()
+
+	assert.True(result)
+	differ.PrettyPrintResult()
+	fmt.Println("============== Test case end: TestLoadSameFile =================")
+}
+
+func TestLoadMismatchedFiles(t *testing.T) {
+	fmt.Println("============== Test case start: TestLoadMismatchedFiles =================")
+	assert := assert.New(t)
+
+	file1 := "/tmp/test1.bin"
+	file2 := "/tmp/test2.bin"
+	defer os.Remove(file1)
+	defer os.Remove(file2)
+
+	entries := 10000
+	numMismatch := 5
+
+	keys, err := genMismatchedFiles(entries, numMismatch, file1, file2)
+	assert.Nil(err)
+
+	differ := NewFilesDiffer(file1, file2)
+	assert.NotNil(differ)
+
+	result := differ.Diff()
+
+	assert.False(result)
+
+	assert.Equal(numMismatch, len(differ.BothExistButMismatch))
+	assert.True(verifyMisMatch(keys, differ))
+
+	assert.Equal(0, len(differ.MissingFromFile1))
+	assert.Equal(0, len(differ.MissingFromFile2))
+
+	differ.PrettyPrintResult()
+	fmt.Println("============== Test case end: TestLoadMismatchedFiles =================")
+}
+
+func TestLoadMismatchedFilesAndUneven(t *testing.T) {
+	fmt.Println("============== Test case start: TestLoadMismatchedFilesAndUneven =================")
+	assert := assert.New(t)
+
+	file1 := "/tmp/test1.bin"
+	file2 := "/tmp/test2.bin"
+	defer os.Remove(file1)
+	defer os.Remove(file2)
+
+	entries := 1000
+	numMismatch := 5
+	extraEntries := 2
+
+	keys, err := genMismatchedFiles(entries, numMismatch, file1, file2)
+	assert.Nil(err)
+
+	// Add more records to one file
+	extraSliceOfPizza := genMultipleRecords(extraEntries)
+	f, err := os.OpenFile(file1, os.O_APPEND|os.O_WRONLY, 644)
+	assert.Nil(err)
+	_, err = f.Write(extraSliceOfPizza)
+	assert.Nil(err)
+	f.Close()
+
+	differ := NewFilesDiffer(file1, file2)
+	assert.NotNil(differ)
+
+	result := differ.Diff()
+
+	assert.False(result)
+
+	assert.Equal(numMismatch, len(differ.BothExistButMismatch))
+	assert.True(verifyMisMatch(keys, differ))
+
+	assert.Equal(0, len(differ.MissingFromFile1))
+	assert.Equal(extraEntries, len(differ.MissingFromFile2))
+	differ.PrettyPrintResult()
+	fmt.Println("============== Test case start: TestLoadMismatchedFilesAndUneven =================")
+}
